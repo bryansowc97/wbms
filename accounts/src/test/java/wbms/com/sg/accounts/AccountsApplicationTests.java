98a92a3d100ab0e208190637cf39249e3daa1f4b
Bryan <bryansowc@gmail.com>
2023-09-27 15:23:00 +0800

    created accounts service

---
+++ accounts/src/test/java/wbms/com/sg/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package wbms.com.sg.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
