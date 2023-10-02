3a5b2793ed8dbc400dbb2db9185d396cb31abe5e
xinying <schonying.lean@ncs.com.sg>
2023-10-02 16:54:26 +0800

    merge conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-
-@NgModule({
-  declarations: [
-    AppComponent
-=======
 import { WorkspaceComponent } from './workspace/workspace.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WorkspaceComponent
->>>>>>> setup_xy
   ],
   imports: [
     BrowserModule,
