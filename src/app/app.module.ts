a599baddf8f0dc289e83d28adc57ad845f58e27b
xinying <schonying.lean@ncs.com.sg>
2023-10-03 12:02:22 +0800

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatIconModule } from "@angular/material/icon";
+import { MatTabsModule } from '@angular/material/tabs';
 
 
 @NgModule({
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    MatToolbarModule,
+    MatIconModule,
+    MatTabsModule 
   ],
   providers: [],
   bootstrap: [
