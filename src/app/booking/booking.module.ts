cc69de5bc3ea9e4f25a94b13733d34227951798d
xinying <schonying.lean@ncs.com.sg>
2023-10-04 14:02:13 +0800

    margin for body

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -4,18 +4,15 @@ import { RouterModule } from "@angular/router";
 import { BOOKING_CHILD_ROUTE } from "./booking.route";
 import { FooterComponent } from "../layout/footer/footer.component";
 import { ButtonModule } from 'primeng/button';
-import { NavbarComponent } from "../layout/navbar/navbar.component";
 import { MenubarModule } from "primeng/menubar";
 
 @NgModule({
     declarations: [
         BookingComponent,
-        FooterComponent,
-        NavbarComponent
+        FooterComponent
     ],
     imports: [
         ButtonModule,
-        MenubarModule,
         RouterModule.forChild(BOOKING_CHILD_ROUTE),
     ],
     exports: [
