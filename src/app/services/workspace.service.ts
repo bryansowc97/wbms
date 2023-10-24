a87306f0fa662383dde49b8dbff280bbc96c4a81
LiangYiNCS <liangyi.pan@ncs.com.sg>
2023-10-24 14:55:54 +0800

    added search bar and date picker for booking dashboard and profile dashboard, also added 8083 for workspace service if running oncompany lappy

--- src/app/services/workspace.service.ts
+++ src/app/services/workspace.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environment';
 
 @Injectable()
 
 export class WorkspaceService {
 
-    apiUrl = 'http://localhost:8082/api/workspace';
+    apiUrl = environment.api.workspace;
     
   constructor(
         private http: HttpClient
